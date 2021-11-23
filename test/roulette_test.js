e63a135c13363d54b871dd3e4871788e7a3f0926
awphi <jellymoos3@gmail.com>
2021-11-23 20:55:18 +0000

    tests

--- test/roulette_test.js
+++ test/roulette_test.js
@@ -1,15 +1,19 @@
+const ethers = require("ethers");
 const Roulette = artifacts.require("Roulette");
+
 contract("Roulette", function (accounts) {
+  beforeEach(async function () {
+    this.roulette = await Roulette.new();
+  });
+
   it("should initially be no bets", async function () {
-    const roulette = await Roulette.deployed();
-    const bets = await roulette.get_bets.call();
+    const bets = await this.roulette.get_bets.call();
     return assert.equal(bets.length, 0);
   });
 
   it("should allow house accounts to spin the wheel", async function () {
-    const roulette = await Roulette.deployed();
     try {
-      await roulette.play.call({ from: accounts[0] });
+      await this.roulette.play.call({ from: accounts[0] });
     } catch (e) {
       return false;
     }
@@ -18,13 +22,32 @@ contract("Roulette", function (accounts) {
   });
 
   it("shouldn't allow non-house accounts to spin the wheel", async function () {
-    const roulette = await Roulette.deployed();
     try {
-      await roulette.play.call({ from: accounts[1] });
+      await this.roulette.play.call({ from: accounts[1] });
     } catch (e) {
       return true;
     }
 
     return false;
+  });
+
+  it("shouldn't allow double-spinning", async function () {
+    this.roulette.play.call({ from: accounts[0] });
+    try {
+      await this.roulette.play.call({ from: accounts[0] });
+    } catch (e) {
+      return true;
+    }
+
+    return false;
+  });
+
+  it("should allow colour bets", async function () {
+    const betValue = ethers.utils.parseEther("0.1");
+    await this.roulette.deposit.call(Roulette.BetType.COLOUR, 0, {
+      from: accounts[1],
+      value: betValue,
+    });
+    return true;
   });
 });
